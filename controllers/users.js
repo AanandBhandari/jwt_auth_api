b08946f05a59d5946538ff6d7e736ae58a967ee3
AanandBhandari <aanandbhandari143@gmail.com>
2019-04-24 16:47:37 +0545

    signed user using jwt and make private route

--- controllers/users.js
+++ controllers/users.js
@@ -1,5 +1,7 @@
 const mongoose = require('mongoose');
 const User = require('../models/users');
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
 
 const connUri = 'mongodb://localhost:27017/apiTest';
 
@@ -30,4 +32,99 @@ module.exports = {
             }
         });
     },
+    login: (req, res) => {
+        const { name, password } = req.body;
+
+        mongoose.connect(connUri, { useNewUrlParser: true }, (err) => {
+            let result = {};
+            let status = 200;
+            if (!err) {
+                User.findOne({ name }, (err, user) => {
+                    if (!err && user) {
+                        // We could compare passwords in our model instead of below as well
+                        bcrypt.compare(password, user.password).then(match => {
+                            if (match) {
+                                status = 200;
+                                // Create a token
+                                const payload = { user: user.name };
+                                const options = { expiresIn: '2d', issuer: 'true' };
+                                // const secret = process.env.JWT_SECRET;
+                                const secret = 'jesus!'
+                                const token = jwt.sign(payload, secret, options);
+
+                                // console.log('TOKEN', token);
+                                result.token = token;
+                                result.status = status;
+                                result.result = user;
+                            } else {
+                                status = 401;
+                                result.status = status;
+                                result.error = `Authentication error`;
+                            }
+                            res.status(status).send(result);
+                        }).catch(err => {
+                            status = 500;
+                            result.status = status;
+                            result.error = err;
+                            res.status(status).send(result);
+                        });
+                    } else {
+                        status = 404;
+                        result.status = status;
+                        result.error = err;
+                        res.status(status).send(result);
+                    }
+                });
+            } else {
+                status = 500;
+                result.status = status;
+                result.error = err;
+                res.status(status).send(result);
+            }
+        });
+    },
+    // getAll: (req, res) => {
+    //     mongoose.connect(connUri, { useNewUrlParser: true }, (err) => {
+    //         User.find({}, (err, users) => {
+    //             if (!err) {
+    //                 res.send(users);
+    //             } else {
+    //                 console.log('Error', err);
+    //             }
+    //         });
+    //     });
+    // }
+    getAll: (req, res) => {
+        mongoose.connect(connUri, { useNewUrlParser: true }, (err) => {
+            let result = {};
+            let status = 200;
+            if (!err) {
+                const payload = req.decoded;
+                if (payload && payload.user === 'true') {
+                    User.find({}, (err, users) => {
+                        if (!err) {
+                            result.status = status;
+                            result.error = err;
+                            result.result = users;
+                        } else {
+                            status = 500;
+                            result.status = status;
+                            result.error = err;
+                        }
+                        res.status(status).send(result);
+                    });
+                } else {
+                    status = 401;
+                    result.status = status;
+                    result.error = `Authentication error`;
+                    res.status(status).send(result);
+                }
+            } else {
+                status = 500;
+                result.status = status;
+                result.error = err;
+                res.status(status).send(result);
+            }
+        });
+    }
 }
